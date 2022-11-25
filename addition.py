e4e6d9576156c7c63500c7068bd7e8533cc11adc
siva <sivakumar.boyina@gmail.com>
2022-11-25 18:36:02 +0530

    my first commit

---
+++ addition.py
@@ -0,0 +1,17 @@
+a=10
+b=25
+c=30
+#addition
+print("addition:",a+b)
+#subtraction
+print("subtraction:",a-b)
+# multiplication
+print("multipliction:",a*b)
+#divsion
+print("division:", b/a)
+#modulus
+print("modulus:", b%a)
+#exponent
+print("exponent:",a**2)
+#floor devision
+print ("floor division:", b//a)
